673337eb027c662c70e74426a605a8bb2354611b
KilianAlves <alveskilian@gmail.com>
2024-05-23 16:23:30 +0200

    ajout de requete api

--- src/services/api.js
+++ src/services/api.js
@@ -9,3 +9,36 @@ export function getCart() {
     response.json(),
   );
 }
+
+export function postCart(article) {
+  // post d'un article dans le panier, quantité 1 avec prix de l'article
+  return fetch(`http://localhost:7000/cart/${article.id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: article.id, quantity: 1, price: article.price }),
+  });
+}
+
+export function deleteCart(article) {
+  return fetch(`http://localhost:7000/cart/${article.id}`, {
+    method: "DELETE",
+  });
+}
+
+export function updateCart(article) {
+  return fetch(`http://localhost:7000/cart/${article.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ quantity: article.quantity }),
+  });
+}
+
+export function clearCart() {
+  return fetch(`http://localhost:7000/cart/`, {
+    method: "DELETE",
+  });
+}
